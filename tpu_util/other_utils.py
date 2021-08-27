b4e24012be75a59d85b6e096837f058838d77713
Wabinab <chowjunwei37@gmail.com>
2021-08-27 09:27:12 +0000

    preliminary test cases WIP

--- tpu_util/other_utils.py
+++ tpu_util/other_utils.py
@@ -5,13 +5,28 @@ requiring TPU to run.
 Separating them allow tests to be run. 
 Created on: 27 August 2021.
 """
+import copy
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.utils.data as D
+from torch.optim import lr_scheduler
+
+from pathlib import Path
+from tqdm.auto import tqdm
 
 # %% [code] {"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2021-08-25T05:58:03.379271Z","iopub.execute_input":"2021-08-25T05:58:03.379563Z","iopub.status.idle":"2021-08-25T05:58:03.388903Z","shell.execute_reply.started":"2021-08-25T05:58:03.379535Z","shell.execute_reply":"2021-08-25T05:58:03.387263Z"}}
-def dataloader(train_ds, val_ds, flags, distributed=False):
+def dataloader(train_ds, val_ds, flags, test_ds=None, distributed=False):
     """
     flags requirement: (python dict)
         "bs": (int) batch_size,
         "num_workers": (int) number of workers.
+
+    :args have_test: (bool) return test dataloader? Default: False
+        If True: requires test_ds to be defined. 
     """
     if distributed:
         train_sampler = D.distributed.DistributedSampler(
@@ -27,4 +42,8 @@ def dataloader(train_ds, val_ds, flags, distributed=False):
                              num_workers=flags["num_workers"],
                              drop_last=True if distributed else False)
     
-    return {"train": train_loader, "val": val_loader}
+    if not test_ds: test_loader = None
+    else: test_loader = D.DataLoader(test_ds, batch_size=flags["bs"], shuffle=False, 
+                                    num_workers=flags["num_workers"])
+    
+    return {"train": train_loader, "val": val_loader, "test": test_loader}
